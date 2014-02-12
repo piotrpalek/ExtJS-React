2644889ce9a4a8a7909d5e3602af6d37350576b9
Piotr Palek <piotrpalek@gmail.com>
2014-02-12 20:18:17 +0100

    the real one

--- Cmp.js
+++ Cmp.js
@@ -1,1 +1,19 @@
-../../Ws/react/Cmp.js
+Ext.define('Ws.react.Cmp', {
+    extend: 'Ext.Component',
+    reactCached: null,
+    initComponent: function() {
+        if(Ext.isObject(this.react)){
+            this.reactCached = React.createClass(this.react);
+            this.mon(this, 'render', this.renderReact, this);
+            this.mon(this, 'beforedestroy', this.destroyReact, this);
+        }
+        this.callParent(arguments);
+    },
+    renderReact: function () {
+        var target = this.getEl();
+        React.renderComponent(this.reactCached(), target.dom);
+    },
+    destroyReact: function () {
+        React.unmountComponentAtNode(this.getEl());
+    }
+});
