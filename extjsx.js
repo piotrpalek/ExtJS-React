a1651604b50ce7800b85561ea76decada82aef9a
Piotr Palek <piotrpalek@gmail.com>
2014-02-12 20:03:52 +0100

    automatically inject jsx header into matched files (probably bad idea, but convenient for dev)

--- extjsx.js
+++ extjsx.js
@@ -1,6 +1,44 @@
 (function(){
 var orgGetPath = Ext.Loader.getPath,
-    orgloadScriptFile = Ext.Loader.loadScriptFile;
+    orgloadScriptFile = Ext.Loader.loadScriptFile,
+    transformer = JSXTransformer;
+
+// modified load method from JSXTransformer
+function load (url, options) {
+  var xhr;
+  xhr = window.ActiveXObject ? new window.ActiveXObject('Microsoft.XMLHTTP')
+                             : new XMLHttpRequest();
+
+  // Disable async since we need to execute scripts in the order they are in the
+  // DOM to mirror normal script loading.
+  xhr.open('GET', url, false);
+  if ('overrideMimeType' in xhr) {
+    xhr.overrideMimeType('text/plain');
+  }
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 0 || xhr.status === 200) {
+        var responseText = xhr.responseText;
+
+        if(options && options.beforeRun){
+          responseText = options.beforeRun(responseText);
+        }
+
+        transformer.run(responseText);
+      } else {
+        throw new Error("Could not load " + url);
+      }
+      if (options && options.callback) {
+        return options.callback(responseText);
+      }
+    }
+  };
+  return xhr.send(null);
+}
+
+function beforeLoadFn (responseText) {
+  return "/** @jsx React.DOM */" + responseText;
+}
 
 Ext.Loader.getPath = function (className) {
   this.LAST_CLASS_NAME = className;
@@ -23,9 +61,11 @@ Ext.Loader.loadScriptFile = function (url, onLoad, onError, scope, synchronous) 
   Ext.Loader.isLoading = true;
 
   if(scope.LAST_CLASS_NAME.indexOf('react') !== -1){
-      var transformer = JSXTransformer;
       try {
-          transformer.load(noCacheUrl, function () {onLoad.call(scope); });
+          load(noCacheUrl, {
+            beforeRun: beforeLoadFn,
+            callback: function () { onLoad.call(scope); }
+          });
       }
       catch(e){
           onError.call(scope);
